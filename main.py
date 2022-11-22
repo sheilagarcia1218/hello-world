765a62d0ac4ac4017cab5994576bcbe4b288514f
sheilagarcia121 <113448445+sheilagarcia1218@users.noreply.github.com>
2022-11-22 15:37:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#this is my first python program
+"""
+this is code is written un python 3.8 interpreted by the console.
+"""
+print("hello world")
+print (50)
+print (3.10)
+print (50,"letters",3.10)
+print ("hello", end = " ")
+print ("world ")
